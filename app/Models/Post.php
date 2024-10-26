aba388f08364f32e90461854898eb33ef5b81bb6
FawwasSabil25 <fawwas2573@gmail.com>
2024-10-27 02:24:26 +0700

    Vid7_Model

---
+++ app/Models/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post{
+    public static function all(){
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Fawwas Sabil',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem minus tenetur aut est error ducimus nulla quo iure odit, amet animi? Quae, sunt recusandae quis voluptatem libero autem neque iusto.'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Fawwas Sabil',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat vel vitae reiciendis pariatur, et tempora? Iusto architecto quaerat deleniti, dolorum molestiae placeat, harum adipisci minima, voluptate reiciendis ullam obcaecati blanditiis?'
+            ]
+        ];
+    }
+
+    public static function find($slug){
+
+        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
+        
+        if(! $post){
+            abort(404);
+        }
+
+        return $post;
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title'=>'Homepage']);
@@ -12,52 +14,16 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', [
-        'title' => 'Blog Page',
-        'posts' => [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Fawwas Sabil',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem minus tenetur aut est error ducimus nulla quo iure odit, amet animi? Quae, sunt recusandae quis voluptatem libero autem neque iusto.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Fawwas Sabil',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat vel vitae reiciendis pariatur, et tempora? Iusto architecto quaerat deleniti, dolorum molestiae placeat, harum adipisci minima, voluptate reiciendis ullam obcaecati blanditiis?'
-            ]
-        ]
-    ]);
+    return view('posts', ['title' => 'Blog page', 'posts' => Post::all()]);
 });
 
 route::get('/posts/{slug}', function($slug){
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Fawwas Sabil',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem minus tenetur aut est error ducimus nulla quo iure odit, amet animi? Quae, sunt recusandae quis voluptatem libero autem neque iusto.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Fawwas Sabil',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat vel vitae reiciendis pariatur, et tempora? Iusto architecto quaerat deleniti, dolorum molestiae placeat, harum adipisci minima, voluptate reiciendis ullam obcaecati blanditiis?'
-        ]
-    ];
 
-    $post = Arr::first($posts, function($post) use ($slug){
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/contact', function () {
+Route::get('/contact', function () {    
     return view('contact', ['title'=>'Contacts']);
 });
