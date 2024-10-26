a7bd1a2ccfbaed82a8575ae50178d835f4957228
FawwasSabil25 <fawwas2573@gmail.com>
2024-10-27 01:31:41 +0700

    Vid6_View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,51 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Fawwas Sabil', 'title'=>'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title'=>'Blog']);
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'Blog Page',
+        'posts' => [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Fawwas Sabil',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem minus tenetur aut est error ducimus nulla quo iure odit, amet animi? Quae, sunt recusandae quis voluptatem libero autem neque iusto.'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Fawwas Sabil',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat vel vitae reiciendis pariatur, et tempora? Iusto architecto quaerat deleniti, dolorum molestiae placeat, harum adipisci minima, voluptate reiciendis ullam obcaecati blanditiis?'
+            ]
+        ]
+    ]);
+});
+
+route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Fawwas Sabil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem minus tenetur aut est error ducimus nulla quo iure odit, amet animi? Quae, sunt recusandae quis voluptatem libero autem neque iusto.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Fawwas Sabil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat vel vitae reiciendis pariatur, et tempora? Iusto architecto quaerat deleniti, dolorum molestiae placeat, harum adipisci minima, voluptate reiciendis ullam obcaecati blanditiis?'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
