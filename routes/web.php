8f5d45f83e438c37c98e5a772b6dee23a59ead3e
FawwasSabil25 <fawwas2573@gmail.com>
2024-10-29 21:17:52 +0700

    Vid11_Eloquent Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +21,11 @@ Route::get('/posts', function () {
 });
 
 route::get('/posts/{post:slug}', function(Post $post){
-
-    // $post = Post::find($id);
-
-
-
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+route::get('/authors/{user}', function(User $user){
+    return view('posts', ['title' => 'Articles By ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/contact', function () {    
