bd4559245d3ccd8fa6d3ae9887ded87af29d4881
FawwasSabil25 <fawwas2573@gmail.com>
2024-10-30 02:45:11 +0700

    vid12_Post Category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +21,15 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog page', 'posts' => Post::all()]);
 });
 
-route::get('/posts/{post:slug}', function(Post $post){
+Route::get('/posts/{post:slug}', function(Post $post){
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-route::get('/authors/{user}', function(User $user){
-    return view('posts', ['title' => 'Articles By ' . $user->name, 'posts' => $user->posts]);
+Route::get('/authors/{user:username}', function(User $user){
+    return view('posts', ['title' => count($user->posts) . ' Articles By ' . $user->name, 'posts' => $user->posts]);
+});
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('posts', ['title' => 'Articles in : ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {    
